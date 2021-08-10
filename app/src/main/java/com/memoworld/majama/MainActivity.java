fd681276af13648144e9dd70d5602a2f584a68cf
sidd2203 <siddharthch22@gmail.com>
2021-08-10 16:50:11 +0530

    second

--- app/src/main/java/com/memoworld/majama/MainActivity.java
+++ app/src/main/java/com/memoworld/majama/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    int a=1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
