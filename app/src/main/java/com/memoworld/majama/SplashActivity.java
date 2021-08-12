45591fe8be8d0072739c89ed8922bf4e3783e40a
Ayush <ayushjaindeoli01@gmail.com>
2021-08-12 21:39:47 +0530

    Added a comment

--- app/src/main/java/com/memoworld/majama/SplashActivity.java
+++ app/src/main/java/com/memoworld/majama/SplashActivity.java
@@ -1,8 +1,9 @@
 package com.memoworld.majama;
 
 import android.content.Intent;
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
