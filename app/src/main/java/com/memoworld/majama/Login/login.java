62c3b1390b4e8e5b11d2eb556569fd10125f495f
Ayush <ayushjaindeoli01@gmail.com>
2021-08-14 11:10:54 +0530

    Refurnished SignUp Activity

--- app/src/main/java/com/memoworld/majama/Login/login.java
+++ app/src/main/java/com/memoworld/majama/Login/login.java
@@ -21,7 +21,7 @@ public class login extends AppCompatActivity {
     ActivityLoginBinding binding;
     Button loginBtn , signup;
     ImageView logo;
-    TextInputLayout username , password;
+    TextInputLayout email , password;
     TextView name;
 
     @Override
@@ -42,7 +42,7 @@ public class login extends AppCompatActivity {
         signup = findViewById(R.id.newUser);
         logo = findViewById(R.id.logoImage);
 
-        username = findViewById(R.id.username);
+        email = findViewById(R.id.userEmail);
         password = findViewById(R.id.password);
 
         name = findViewById(R.id.logoName);
@@ -55,7 +55,7 @@ public class login extends AppCompatActivity {
                 Pair[] pairs = new Pair[6];
                 pairs[0] = new Pair<View,String>(logo , "logo_image");
                 pairs[1] = new Pair<View,String>(name , "app_name");
-                pairs[2] = new Pair<View,String>(username , "user_trans");
+                pairs[2] = new Pair<View,String>(email , "user_mail");
                 pairs[3] = new Pair<View,String>(password , "pass_trans");
                 pairs[4] = new Pair<View,String>(loginBtn , "login_trans");
                 pairs[5] = new Pair<View,String>(signup , "sign_trans");
