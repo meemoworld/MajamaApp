9159c6d02ac316c5c5b95773930abdd3856472c6
Ayush <ayushjaindeoli01@gmail.com>
2021-09-02 16:22:02 +0530

    completed single user chat activity

---
+++ app/src/main/java/com/memoworld/majama/Chats/sample_showUser.java
@@ -0,0 +1,18 @@
+package com.memoworld.majama.Chats;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.memoworld.majama.R;
+
+public class sample_showUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample_show_user);
+
+
+    }
+}
