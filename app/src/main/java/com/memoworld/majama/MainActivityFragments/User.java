c4de07380b2830409999c5f6453331a3a019e210
sidd2203 <siddharthch22@gmail.com>
2021-09-02 16:45:31 +0530

    User Fragment

--- app/src/main/java/com/memoworld/majama/MainActivityFragments/User.java
+++ app/src/main/java/com/memoworld/majama/MainActivityFragments/User.java
@@ -1,7 +1,5 @@
 package com.memoworld.majama.MainActivityFragments;
 
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -11,7 +9,8 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.memoworld.majama.R;
- public class User extends Fragment {
+
+public class User extends Fragment {
 
 
     public User() {
@@ -28,4 +27,20 @@ import com.memoworld.majama.R;
 
         return view;
     }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        MenuInflater menuInflater = getMenuInflater();
+//        menuInflater.inflate(R.menu.profile_activity_menu, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+//        if (item.getItemId() == R.id.profile_activity_menu) {
+//            startActivity(new Intent(ProfileActivity.this, userSetting.class));
+//            return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
 }
